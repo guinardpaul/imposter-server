55640663cf77b9505c6fd1e5e4492a84bf0f5847
guinardpaul <guinardpaul@gmail.com>
2026-02-01 17:06:45 +0100

    Init project

---
+++ src/main/java/com/guinardpouard/imposteur/ImposteurApplication.java
@@ -0,0 +1,13 @@
+package com.guinardpouard.imposteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImposteurApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ImposteurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guinardpouard/imposteur/ImposteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guinardpouard.imposteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImposteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
